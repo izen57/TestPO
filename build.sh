21550ac9b1160db8b25a839897403a4a21e82880
kmd19u787 <golovoslon@gmail.com>
2022-11-27 17:08:56 +0300

    ci/cd fix

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-msbuild IntegrationTests\IntegrationTests.csproj
-msbuild UnitTests\UnitTests.csproj
+msbuild IntegrationTests/IntegrationTests.csproj
+msbuild UnitTests/UnitTests.csproj
 
-vstest.console.exe /logger:trx IntegrationTests\bin\debug\net6.0-windows10.0.22621.0\IntegrationTests.dll
-vstest.console.exe /logger:trx UnitTests\bin\debug\net6.0-windows10.0.22621.0\UnitTests.dll
-allure serve TestResults\
+vstest.console.exe /logger:trx IntegrationTests/bin/debug/net6.0-windows10.0.22621.0/IntegrationTests.dll
+vstest.console.exe /logger:trx UnitTests/bin/debug/net6.0-windows10.0.22621.0/UnitTests.dll
+allure serve TestResults/
 
 #rmdir /s /q %LOCALAPPDATA%\IsolatedStorage
-rmdir -s -q %LOCALAPPDATA%\IsolatedStorage
+rmdir -s -q %LOCALAPPDATA%/IsolatedStorage
