f3e8f3aa94f3b2e7f9c8537e0baea84347869b83
kmd19u787 <golovoslon@gmail.com>
2022-11-27 17:00:24 +0300

    build fix

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 msbuild IntegrationTests\IntegrationTests.csproj
 msbuild UnitTests\UnitTests.csproj
 
@@ -5,4 +7,5 @@ vstest.console.exe /logger:trx IntegrationTests\bin\debug\net6.0-windows10.0.226
 vstest.console.exe /logger:trx UnitTests\bin\debug\net6.0-windows10.0.22621.0\UnitTests.dll
 allure serve TestResults\
 
-rmdir /s /q %LOCALAPPDATA%\IsolatedStorage
+#rmdir /s /q %LOCALAPPDATA%\IsolatedStorage
+rmdir -s -q %LOCALAPPDATA%\IsolatedStorage
