55a645382b011b90fe91bd866ed94edfa9a2fe7a
kmd19u787 <golovoslon@gmail.com>
2022-11-27 22:11:19 +0300

    build fix

--- build.bat
+++ build.bat
@@ -1,8 +1,4 @@
-msbuild IntegrationTests\IntegrationTests.csproj
-msbuild UnitTests\UnitTests.csproj
-
-vstest.console.exe /logger:trx IntegrationTests\bin\debug\net6.0-windows10.0.22621.0\IntegrationTests.dll
-vstest.console.exe /logger:trx UnitTests\bin\debug\net6.0-windows10.0.22621.0\UnitTests.dll
+dotnet test -l trx
 allure serve TestResults\
 
 rmdir /s /q %LOCALAPPDATA%\IsolatedStorage
