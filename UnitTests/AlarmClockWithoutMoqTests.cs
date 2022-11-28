ff1af4801710e85a3ca77f032b0e557b7488b82c
kmd19u787 <golovoslon@gmail.com>
2022-11-28 16:51:56 +0300

    special fail

--- UnitTests/AlarmClockWithoutMoqTests.cs
+++ UnitTests/AlarmClockWithoutMoqTests.cs
@@ -29,7 +29,7 @@ namespace TestPPO
 			alarmClockService.Edit(check2, dateTime);
 			Assert.IsNotNull(alarmClockService.GetAlarmClock(dateTime), "AlarmClockEdit");
 
-			alarmClockService.Delete(dateTime);
+			alarmClockService.Delete(new(2022, 9, 14, 19, 15, 1));
 			Assert.AreEqual(0, alarmClockService.GetAllAlarmClocks().Count, "AlarmClockDelete");
 		}
 	}
